1132ae82e0d47699f7a1528b52427e3a09499c6e
songnathan <yohees213@gmail.com>
2024-12-07 20:10:55 +0900

    필요없는 부분 수정

--- backend/app.py
+++ backend/app.py
@@ -11,5 +11,4 @@ api.add_resource(Test, '/test')
 
 # app.run() 호출 제거 (PythonAnywhere에서 WSGI로 실행되므로)
 if __name__ == '__main__':
-    # 로컬에서 실행할 때만 사용, PythonAnywhere에서는 필요 없음
     app.run(debug=True)
